dba89b79d45d8c6be3096d8fba59f3136d871476
ostap-mykhaylyak <me@ostap.dev>
2026-08-04 12:13:28 +0200

    Versione 0.1.3

--- internal/version/version.go
+++ internal/version/version.go
@@ -4,7 +4,7 @@ package version
 // Version viene sovrascritta in fase di build con -ldflags.
 // Deve restare allineata a PHP_ORMA_VERSION in ext/php_orma.h: l'estensione e il
 // daemon si rifiutano di parlarsi se le versioni maggiori divergono.
-var Version = "0.1.2"
+var Version = "0.1.3"
 
 // Commit viene sovrascritto in fase di build con -ldflags.
 var Commit = "sviluppo"
